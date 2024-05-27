2d10289642bcfbd8e84e67c91021fb53dc2c39d4
Pedro Pires <96807047+pedromigueldmn@users.noreply.github.com>
2024-05-27 22:39:26 +0100

    no hillclimbing_1 já não dá false

--- MedAI/PARTE2/hillclimbing_1.pl
+++ MedAI/PARTE2/hillclimbing_1.pl
@@ -1,76 +1,123 @@
-% Definição dos arcos com tempos (dias) e custos (€)
-arco(s0, s1, 3, 100).
-arco(s0, s2, 4, 200).
-arco(s0, s3, 2, 150).
-arco(s1, s4, 1, 50).
-arco(s1, s5, 3, 100).
-arco(s2, s5, 2, 100).
-arco(s3, s6, 1, 50).
-arco(s4, s7, 2, 150).
-arco(s5, s7, 1, 100).
-arco(s6, s8, 2, 200).
-arco(s7, s8, 1, 50).
-arco(s8, s9, 3, 100).
+% Definição dos arcos e tempos (dias/sessões) baseados na tabela fornecida
+arco(s1, s2, 2).
+arco(s1, s3, 3).
+arco(s1, s4, 4).
+arco(s1, s5, 5).
+arco(s1, s6, 2).
+arco(s2, s6, 2).
+arco(s2, s7, 6).
+arco(s2, s0, 6).
+arco(s2, s8, 8).
+arco(s3, s6, 2).
+arco(s3, s7, 6).
+arco(s3, s9, 12).
+arco(s3, s15, 15).
+arco(s4, s6, 2).
+arco(s4, s11, 10).
+arco(s4, s12, 30).
+arco(s4, s0, 30).
+arco(s4, s13, 8).
+arco(s4, s9, 30).
+arco(s5, s6, 2).
+arco(s5, s0, 8).
+arco(s5, s13, 8).
+arco(s6, s20, 20).
+arco(s6, s0, 20).
+arco(s7, s10, 3).
+arco(s7, s17, 90).
+arco(s8, s17, 90).
+arco(s9, s0, 30).
+arco(s9, s17, 90).
+arco(s10, s0, 3).
+arco(s11, s0, 10).
+arco(s12, s18, 3).
+arco(s12, s0, 10).
+arco(s13, s0, 8).
+arco(s15, s0, 15).
+arco(s17, s18, 3).
+arco(s18, s0, 6).
 
-% Atribuição de custos
-custo(X, Y, C) :- arco(X, Y, _, C).
+% Definição dos custos (€) baseados na tabela fornecida
+custo(s1, 0).
+custo(s2, 5).
+custo(s3, 4).
+custo(s4, 2).
+custo(s5, 8).
+custo(s6, 100).
+custo(s7, 150).
+custo(s8, 200).
+custo(s9, 0).
+custo(s10, 1200).
+custo(s11, 50).
+custo(s12, 34).
+custo(s13, 25).
+custo(s15, 90).
+custo(s17, 80).
+custo(s18, 75).
+custo(s20, 15).
 
-% Atribuição de tempos
-tempo(X, Y, T) :- arco(X, Y, T, _).
-
-% Avaliação de uma solução: soma de custos e tempos
+% Função para calcular o custo total de um caminho
 eval([_], 0).
-eval([D, O | X], C) :- 
-    custo(O, D, C1), 
-    tempo(O, D, T1), 
-    eval([O | X], C2), 
-    C is C1 + T1 + C2.
+eval([D, O | X], C) :-
+    custo(O, C1),
+    eval([O | X], C2),
+    C is C1 + C2.
 
-origem(s0).
-destino(s9).
+% Definir a origem e o destino
+origem(s1).       %dizer s1 e dizer start é a mesma coisa
+destino(s0).
 
-change(S, SNEW) :- 
-    origem(O), 
-    destino(D), 
-    findall(X, caminho(O, D, X), L), 
+% Função para encontrar novos caminhos
+change(S, SNEW) :-
+    origem(O), destino(D),
+    findall(X, caminho(O, D, X), L),
     length(L, LE), 
     random(0, LE, R), 
-    nth0(R, L, SNEW), 
+    nth0(R, L, SNEW),
     dif(SNEW, S).
-change(S, SNEW) :- change(S, SNEW).
+change(S, SNEW) :-
+    change(S, SNEW).
 
-demo :- 
-    origem(O), 
-    destino(D), 
-    caminho(O, D, C), 
-    hill_climbing(C, [20, 1, 0, min], S), 
-    write(S).
+% Demonstração do algoritmo
+demo :-
+    origem(O), destino(D), caminho(O, D, C),
+    hill_climbing(C, [20, 1, 0, min], S), write(S).
 
-% Definição de caminho
+% Definição dos caminhos
 caminho(X, Z, C) :- caminho(X, Z, [X], C).
 caminho(X, X, Caminho, Caminho).
 caminho(X, Z, Visitado, Caminho) :-
-    arco(X, Y, _, _),
+    arco(X, Y, _),
     \+ member(Y, Visitado),
     caminho(Y, Z, [Y | Visitado], Caminho).
 
-% Função best
+% Função para determinar a melhor solução
 best(Prob, Opt, S1, E1, S2, E2, SR, ER) :-
     eval(S2, E2),
     best_opt(Prob, Opt, S1, E1, S2, E2, SR, ER).
 
-best_opt(Prob, _, _, _, S2, E2, S2, E2) :- random(X), X < Prob.
-best_opt(_, Opt, S1, E1, S2, E2, SR, ER) :-
-    (Opt = max, max_list([E1, E2], ER); Opt = min, min_list([E1, E2], ER)),
-    (ER == E1, SR = S1; ER == E2, SR = S2).
+best_opt(Prob, _, _, _, S2, E2, S2, E2) :-
+    random(X), 
+    X < Prob.
 
-% Função show
-show(final, Verbose, S1, E1, _, _) :- Verbose > 0, write('final:'), write(' S:'), write(S1), write(' E:'), write(E1), nl.
-show(0, Verbose, S1, E1, _, _) :- Verbose > 0, write('init:'), write(' S0:'), write(S1), write(' E0:'), write(E1), nl.
-show(I, Verbose, S1, E1, S2, E2) :- 0 is I mod Verbose, write('iter:'), write(I), write(' S1:'), write(S1), write(' E1:'), write(E1), write(' S2:'), write(S2), write(' E2:'), nl.
+best_opt(_, Opt, S1, E1, S2, E2, SR, ER) :-
+    ((Opt = max, max_list([E1, E2], ER)); (Opt = min, min_list([E1, E2], ER))),
+    ((ER == E1, SR = S1); (ER == E2, SR = S2)).
+
+% Mostrar evolução
+show(final, Verbose, S1, E1, _, _) :-
+    Verbose > 0,
+    write('final:'), write(' S:'), write(S1), write(' E:'), write(E1), nl.
+show(0, Verbose, S1, E1, _, _) :-
+    Verbose > 0,
+    write('init:'), write(' S0:'), write(S1), write(' E0:'), write(E1), nl.
+show(I, Verbose, S1, E1, S2, E2) :-
+    0 is I mod Verbose,
+    write('iter:'), write(I), write(' S1:'), write(S1), write(' E1:'),
+    write(E1), write(' S2:'), write(S2), write(' E2:'), write(E2), nl.
 show(_, _, _, _, _, _).
 
-% Função hill climbing
+% Algoritmo de Hill Climbing
 hill_climbing(S0, [Iter, Verbose, Prob, Opt], S1) :-
     eval(S0, E0),
     show(0, Verbose, S0, E0, _, _),
