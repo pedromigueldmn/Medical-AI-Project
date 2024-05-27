1247b875345b95fc8d0a4fb587a16dc174cf4522
Pedro Pires <96807047+pedromigueldmn@users.noreply.github.com>
2024-05-27 21:03:52 +0100

    .

--- MedAI/PARTE2/hillclimbing.pl
+++ MedAI/PARTE2/hillclimbing.pl
@@ -1,97 +1,75 @@
 % Carregar a base de dados
 :- consult('base_dados.pl').
 
-% Definir os predicados caminho/4 e caminho/3
+% Predicado para listar caminhos de start a end, evitando ciclos
 caminho(X, Z, Caminho) :- caminho(X, Z, [X], Caminho).
 caminho(X, X, CaminhoAcumulado, Caminho) :- reverse(CaminhoAcumulado, Caminho).
 caminho(X, Z, CaminhoAcumulado, Caminho) :- 
-    percurso(X, Y, _), 
-    \+ member(Y, CaminhoAcumulado), 
+    percurso(X, Y, _),
+    \+ member(Y, CaminhoAcumulado),
     caminho(Y, Z, [Y | CaminhoAcumulado], Caminho).
 
-% Predicado para calcular o custo total de um caminho
+% Calcula o custo total de um caminho
 custoTotal([], 0).
 custoTotal([X|R], Soma) :- 
-    tratamento(X, CustoTratamentoIndividual), 
-    custoTotal(R, TotalCusto), 
-    Soma is CustoTratamentoIndividual + TotalCusto.
+    tratamento(X, CustoTratamentoIndividual),
+    custoTotal(R, TotalCusto),
+    Soma is TotalCusto + CustoTratamentoIndividual.
 
-% Predicado para calcular o tempo total de um caminho
+% Calcula o tempo total de um caminho
 calcularTempo(_, [], 0).
 calcularTempo(P1, [P2|R2], Soma) :- 
-    percurso(P1, P2, TempoTratamentoIndividual), 
-    calcularTempo(P2, R2, TempoTotal), 
-    Soma is TempoTratamentoIndividual + TempoTotal.
+    percurso(P1, P2, TempoTratamentoIndividual),
+    calcularTempo(P2, R2, TempoTotal),
+    Soma is TempoTotal + TempoTratamentoIndividual.
 
-% Predicado eval/2 para avaliar a solução baseada na vantagem (custo + tempo)
+% Avalia um caminho, somando custos e tempos
 eval([_], 0).
 eval([X, Y | Resto], C) :- 
-    percurso(X, Y, C1), 
-    eval([Y | Resto], C2), 
-    C is C1 + C2.
+    tratamento(X, CustoX),
+    percurso(X, Y, TempoXY),
+    eval([Y | Resto], CResto),
+    C is CResto + CustoX + TempoXY.
 
-% Predicado change/2 para gerar uma nova solução a partir da solução atual
+% Gera uma nova opção de caminho
 change(S, SNEW):- 
-    findall(X, caminho(start, end, X), L), 
-    length(L, LE), 
-    random(0, LE, R), 
-    nth0(R, L, SNEW), 
+    findall(X, caminho(start, end, X), L),
+    length(L, LE),
+    random(0, LE, R),
+    nth0(R, L, SNEW),
     dif(SNEW, S).
 
-% Funções auxiliares do Hill Climbing
-best(Prob, Opt, S1, E1, S2, E2, SR, ER):- 
-    eval(S2, E2), 
-    best_opt(Prob, Opt, S1, E1, S2, E2, SR, ER).
+% Escolhe o melhor entre o caminho atual e o novo
+best(Prob, Opt, S1, E1, S2, E2, SFinal, EFinal):- 
+    eval(S2, E2),
+    (   (Opt = max, E2 > E1; Opt = min, E2 < E1),
+        random(Rand), Rand < Prob -> SFinal = S2, EFinal = E2
+    ;   SFinal = S1, EFinal = E1
+    ).
 
-best_opt(Prob, _, _, _, S2, E2, S2, E2):- 
-    random(X), 
-    X < Prob.
-
-best_opt(_, Opt, S1, E1, S2, E2, SR, ER):- 
-    ((Opt = max, max_list([E1, E2], ER)); (Opt = min, min_list([E1, E2], ER))), 
-    ((ER =:= E1, SR = S1); (ER =:= E2, SR = S2)).
-
-% Mostrar evolução do algoritmo
-show(final, Verbose, S1, E1, _, _):- 
+% Mostra o progresso
+show(I, Verbose, S1, E1):- 
     Verbose > 0, 
-    write('final:'), write(' S:'), write(S1), write(' E:'), write(E1), nl, !.
-show(0, Verbose, S1, E1, _, _):- 
-    Verbose > 0, 
-    write('init:'), write(' S0:'), write(S1), write(' E0:'), write(E1), nl, !.
-show(I, Verbose, S1, E1, S2, E2):- 
     0 is I mod Verbose, 
-    write('iter:'), write(I), write(' S1:'), write(S1), 
-    write(' E1:'), write(E1), write(' S2:'), write(S2), 
-    write(' E2:'), write(E2), nl, !.
-show(_, _, _, _, _, _).
+    format('Iter: ~w, Caminho: ~w, Custo: ~w~n', [I, S1, E1]).
 
-% Hill Climbing
-hill_climbing(S0, [Iter, Verbose, Prob, Opt], S1):- 
-    eval(S0, E0), 
-    show(0, Verbose, S0, E0, _, _), 
-    hill_climbing(S0, E0, 0, Iter, Verbose, Prob, Opt, S1).
+% Executa o hill climbing
+hill_climbing(S1, E1, I, MaxIter, Verbose, Prob, Opt, SFinal):- 
+    I >= MaxIter, !, SFinal = S1.
+hill_climbing(S1, E1, I, MaxIter, Verbose, Prob, Opt, SFinal):- 
+    change(S1, S2),
+    best(Prob, Opt, S1, E1, S2, E2, SN, EN),
+    show(I, Verbose, SN, EN),
+    I1 is I + 1,
+    hill_climbing(SN, EN, I1, MaxIter, Verbose, Prob, Opt, SFinal).
 
-hill_climbing(S, _, Iter, Iter, _, _, _, S):- !.
-hill_climbing(S1, E1, I, Iter, Verbose, Prob, Opt, SFinal):- 
-    change(S1, SNew), 
-    best(Prob, Opt, S1, E1, SNew, _, S2, E2), 
-    I1 is I + 1, 
-    show(I1, Verbose, S1, E1, S2, E2), 
-    hill_climbing(S2, E2, I1, Iter, Verbose, Prob, Opt, SFinal), !.
-
-% Exemplo de uso do Hill Climbing
-demo_hill_climbing :- 
-    caminho(start, end, CaminhoInicial), 
-    hill_climbing(CaminhoInicial, [100, 1, 0, min], MelhorCaminho), 
-    custoTotal(MelhorCaminho, Custo), 
-    calcularTempo(start, MelhorCaminho, Tempo), 
-    Vantagem is Custo + Tempo, 
-    write('Melhor Caminho: '), write(MelhorCaminho), nl, 
-    write('Custo: '), write(Custo), nl, 
-    write('Tempo: '), write(Tempo), nl, 
-    write('Vantagem: '), write(Vantagem), nl, !.
-
-
-
-
-% PARA TESTARES, COLOCA O SEGUINTE NO TERMINAL: "  demo_hill_climbing.  "
+% Demo para testar
+demo_hill_climbing :-
+    caminho(start, end, CaminhoInicial),
+    eval(CaminhoInicial, AvaliacaoInicial),
+    hill_climbing(CaminhoInicial, AvaliacaoInicial, 0, 100, 10, 0.05, min, MelhorCaminho),
+    custoTotal(MelhorCaminho, Custo),
+    calcularTempo(start, MelhorCaminho, Tempo),
+    write('Melhor Caminho: '), write(MelhorCaminho), nl,
+    write('Custo: '), write(Custo), nl,
+    write('Tempo: '), write(Tempo), nl.
---
+++ MedAI/PARTE2/hillclimbing_1.pl
@@ -0,0 +1,78 @@
+% Carregar a base de dados fictícia com arcos e custos
+:- initialization(main).
+
+% Definições dos arcos com custos associados
+arco(s0, s1, 480).
+arco(s0, s2, 520).
+arco(s1, s2, 150).
+arco(s1, s3, 200).
+arco(s2, s3, 320).
+% Adicione aqui todos os outros arcos mostrados no seu gráfico
+
+% Custo para se mover entre dois pontos, se não existe ligação direta, custo é considerado infinito
+custo(X, Y, Custo) :- arco(X, Y, Custo); arco(Y, X, Custo).
+custo(_, _, inf).
+
+% Caminho de Origem para Destino, evitando ciclos
+caminho(Origem, Destino, Caminho) :-
+    caminho(Origem, Destino, [Origem], Caminho).
+
+caminho(Origem, Origem, _, [Origem]).
+caminho(Origem, Destino, Visitados, [Origem | Caminho]) :-
+    arco(Origem, Proximo, _),
+    \+ member(Proximo, Visitados),
+    caminho(Proximo, Destino, [Proximo | Visitados], Caminho).
+
+% Avaliação do custo de um caminho
+eval([_], 0).
+eval([Origem, Destino | Resto], CustoTotal) :-
+    custo(Origem, Destino, Custo),
+    eval([Destino | Resto], CustoResto),
+    CustoTotal is Custo + CustoResto.
+
+% Gerar uma nova solução a partir da atual
+change(SolucaoAtual, NovaSolucao) :-
+    findall(X, caminho(s0, s18, X), SolucoesPossiveis),
+    length(SolucoesPossiveis, NumeroSolucoes),
+    random(0, NumeroSolucoes, Index),
+    nth0(Index, SolucoesPossiveis, NovaSolucao),
+    dif(NovaSolucao, SolucaoAtual).
+
+% Hill climbing implementation
+hill_climbing(SolucaoAtual, [Iteracoes, Verbosidade, Probabilidade, Opt], SolucaoFinal) :-
+    eval(SolucaoAtual, AvaliacaoAtual),
+    hill_climbing(SolucaoAtual, AvaliacaoAtual, 0, Iteracoes, Verbosidade, Probabilidade, Opt, SolucaoFinal).
+
+hill_climbing(Solucao, Avaliacao, Iteracoes, Iteracoes, _, _, _, Solucao) :- 
+    format('Final Solution: ~w with Cost: ~w~n', [Solucao, Avaliacao]), !.
+
+hill_climbing(SolucaoAtual, AvaliacaoAtual, I, MaxIter, Verbosidade, Probabilidade, Opt, SolucaoFinal) :-
+    change(SolucaoAtual, NovaSolucao),
+    eval(NovaSolucao, NovaAvaliacao),
+    (   melhor(Opcao, Opt, SolucaoAtual, AvaliacaoAtual, NovaSolucao, NovaAvaliacao, SolucaoEscolhida, AvaliacaoEscolhida),
+        show(I, Verbosidade, SolucaoAtual, AvaliacaoAtual, NovaSolucao, NovaAvaliacao),
+        I1 is I + 1,
+        hill_climbing(SolucaoEscolhida, AvaliacaoEscolhida, I1, MaxIter, Verbosidade, Probabilidade, Opt, SolucaoFinal)
+    ).
+
+% Decidir qual solução é melhor
+melhor(NovaSolucao, min, _, AvaliacaoAtual, NovaSolucao, NovaAvaliacao, NovaSolucao, NovaAvaliacao) :-
+    NovaAvaliacao < AvaliacaoAtual, !.
+
+melhor(SolucaoAtual, min, SolucaoAtual, AvaliacaoAtual, _, _, SolucaoAtual, AvaliacaoAtual).
+
+% Exibir a evolução do processo
+show(Iteracao, Verbosidade, S1, E1, S2, E2) :-
+    Verbosidade > 0,
+    Iteracao mod Verbosidade =:= 0,
+    format('Iteração: ~d~nSolução Atual: ~w, Custo: ~w~nNova Solução: ~w, Novo Custo: ~w~n', [Iteracao, S1, E1, S2, E2]).
+
+% Ponto de entrada para o programa
+main :-
+    demo,
+    halt.
+
+demo :-
+    caminho(s0, s18, CaminhoInicial),
+    hill_climbing(CaminhoInicial, [100, 1, 0.05, min], _),
+    halt.
