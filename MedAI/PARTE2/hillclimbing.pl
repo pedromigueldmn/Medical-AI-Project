2566fea351c60e1a04e01a8a0f98d1461a8e11a8
Pedro Pires <96807047+pedromigueldmn@users.noreply.github.com>
2024-05-27 23:15:54 +0100

    Hillclimbing funcional

--- MedAI/PARTE2/hillclimbing.pl
+++ MedAI/PARTE2/hillclimbing.pl
@@ -1,75 +1,186 @@
-% Carregar a base de dados
-:- consult('base_dados.pl').
+% Definição dos tratamentos e seus custos
+tratamento(start, 0).
+tratamento(paracetamol, 5).
+tratamento(brufen, 4).
+tratamento(aspirina, 2).
+tratamento(anti_histaminico, 8).
+tratamento(descanso, 100).
+tratamento(fisioterapia, 150).
+tratamento(cineterapia, 200).
+tratamento(desporto, 0).
+tratamento(cirurgia, 1200).
+tratamento(inalacoes, 50).
+tratamento(insulina, 34).
+tratamento(antibiotico, 25).
+tratamento(dieta, 0).
+tratamento(ginastica, 90).
+tratamento(internamento, 250).
+tratamento(dialise, 100).
+tratamento(calmante, 80).
+tratamento(injecoes, 75).
+tratamento(inalo_terapia, 90).
+tratamento(soro, 15).
+tratamento(oculos, 500).
+tratamento(lentes_contacto, 300).
+tratamento(ambulatorio, 800).
+tratamento(end, 0).
 
-% Predicado para listar caminhos de start a end, evitando ciclos
-caminho(X, Z, Caminho) :- caminho(X, Z, [X], Caminho).
-caminho(X, X, CaminhoAcumulado, Caminho) :- reverse(CaminhoAcumulado, Caminho).
-caminho(X, Z, CaminhoAcumulado, Caminho) :- 
-    percurso(X, Y, _),
-    \+ member(Y, CaminhoAcumulado),
-    caminho(Y, Z, [Y | CaminhoAcumulado], Caminho).
+% Definição dos arcos e tempos (dias/sessões)
+percurso(start, paracetamol, 2).
+percurso(start, brufen, 3).
+percurso(start, aspirina, 4).
+percurso(start, anti_histaminico, 5).
+percurso(start, descanso, 2).
 
-% Calcula o custo total de um caminho
-custoTotal([], 0).
-custoTotal([X|R], Soma) :- 
-    tratamento(X, CustoTratamentoIndividual),
-    custoTotal(R, TotalCusto),
-    Soma is TotalCusto + CustoTratamentoIndividual.
+percurso(paracetamol, descanso, 2).
+percurso(paracetamol, fisioterapia, 6).
+percurso(paracetamol, cineterapia, 8).
+percurso(paracetamol, antibiotico, 8).
+percurso(paracetamol, oculos, 1000).
+percurso(paracetamol, lentes_contacto, 200).
 
-% Calcula o tempo total de um caminho
-calcularTempo(_, [], 0).
-calcularTempo(P1, [P2|R2], Soma) :- 
-    percurso(P1, P2, TempoTratamentoIndividual),
-    calcularTempo(P2, R2, TempoTotal),
-    Soma is TempoTotal + TempoTratamentoIndividual.
+percurso(brufen, descanso, 2).
+percurso(brufen, fisioterapia, 6).
+percurso(brufen, desporto, 12).
+percurso(brufen, ginastica, 15).
+percurso(brufen, oculos, 1000).
+percurso(brufen, lentes_contacto, 200).
 
-% Avalia um caminho, somando custos e tempos
+percurso(aspirina, inalacoes, 10).
+percurso(aspirina, insulina, 30).
+percurso(aspirina, antibiotico, 8).
+percurso(aspirina, dieta, 30).
+percurso(aspirina, soro, 8).
+percurso(aspirina, oculos, 1000).
+percurso(aspirina, lentes_contacto, 200).
+
+percurso(anti_histaminico, antibiotico, 8).
+percurso(anti_histaminico, internamento, 3).
+
+percurso(descanso, cineterapia, 8).
+percurso(descanso, oculos, 1000).
+percurso(descanso, lentes_contacto, 200).
+
+percurso(fisioterapia, end, 0).
+percurso(fisioterapia, cirurgia, 3).
+percurso(fisioterapia, ginastica, 15).
+percurso(fisioterapia, calmante, 90).
+percurso(fisioterapia, injecoes, 3).
+percurso(fisioterapia, ambulatorio, 20).
+
+percurso(cineterapia, end, 0).
+percurso(cineterapia, calmante, 90).
+
+percurso(desporto, dieta, 30).
+percurso(desporto, calmante, 90).
+
+percurso(cirurgia, internamento, 3).
+percurso(cirurgia, soro, 8).
+
+percurso(inalacoes, soro, 8).
+
+percurso(insulina, dialise, 10).
+percurso(insulina, injecoes, 3).
+
+percurso(antibiotico, internamento, 3).
+
+percurso(dieta, end, 0).
+
+percurso(ginastica, ambulatorio, 20).
+
+percurso(internamento, end, 0).
+
+percurso(dialise, end, 0).
+
+percurso(calmante, ambulatorio, 20).
+
+percurso(injecoes, inalo_terapia, 6).
+percurso(injecoes, ambulatorio, 20).
+
+percurso(inalo_terapia, end, 0).
+
+percurso(soro, ambulatorio, 20).
+
+percurso(oculos, end, 0).
+
+percurso(lentes_contacto, end, 0).
+
+percurso(ambulatorio, end, 0).
+
+% Função para calcular o custo total de um caminho (custo + tempo)
 eval([_], 0).
-eval([X, Y | Resto], C) :- 
-    tratamento(X, CustoX),
-    percurso(X, Y, TempoXY),
-    eval([Y | Resto], CResto),
-    C is CResto + CustoX + TempoXY.
+eval([D, O | X], C) :-
+    tratamento(O, CustoO),
+    percurso(O, D, TempoD),
+    eval([O | X], C2),
+    C is CustoO + TempoD + C2.
 
-% Gera uma nova opção de caminho
-change(S, SNEW):- 
-    findall(X, caminho(start, end, X), L),
-    length(L, LE),
-    random(0, LE, R),
-    nth0(R, L, SNEW),
-    dif(SNEW, S).
+% Definir a origem e o destino
+origem(start).
+destino(end).
 
-% Escolhe o melhor entre o caminho atual e o novo
-best(Prob, Opt, S1, E1, S2, E2, SFinal, EFinal):- 
+% Função para encontrar novos caminhos
+change(S, SNEW) :-
+    origem(O), destino(D),
+    findall(C, caminho(O, D, C), L),
+    select_melhor(L, SNEW).
+
+select_melhor([H], H) :- !.
+select_melhor([H|T], Melhor) :-
+    select_melhor(T, TempMelhor),
+    eval(H, EH),
+    eval(TempMelhor, ET),
+    (EH < ET -> Melhor = H ; Melhor = TempMelhor).
+
+% Demonstração do algoritmo
+demo :-
+    origem(O), destino(D), caminho(O, D, C),
+    hill_climbing(C, [20, 1, 0, min], S), write(S).
+
+% Definição dos caminhos
+caminho(X, Z, C) :- caminho(X, Z, [X], C).
+caminho(X, X, Caminho, Caminho).
+caminho(X, Z, Visitado, Caminho) :-
+    percurso(X, Y, _),
+    \+ member(Y, Visitado),
+    caminho(Y, Z, [Y | Visitado], Caminho).
+
+% Função para determinar a melhor solução
+best(Prob, Opt, S1, E1, S2, E2, SR, ER) :-
     eval(S2, E2),
-    (   (Opt = max, E2 > E1; Opt = min, E2 < E1),
-        random(Rand), Rand < Prob -> SFinal = S2, EFinal = E2
-    ;   SFinal = S1, EFinal = E1
-    ).
+    best_opt(Prob, Opt, S1, E1, S2, E2, SR, ER).
 
-% Mostra o progresso
-show(I, Verbose, S1, E1):- 
-    Verbose > 0, 
-    0 is I mod Verbose, 
-    format('Iter: ~w, Caminho: ~w, Custo: ~w~n', [I, S1, E1]).
+best_opt(Prob, _, _, _, S2, E2, S2, E2) :-
+    random(X), 
+    X < Prob.
 
-% Executa o hill climbing
-hill_climbing(S1, E1, I, MaxIter, Verbose, Prob, Opt, SFinal):- 
-    I >= MaxIter, !, SFinal = S1.
-hill_climbing(S1, E1, I, MaxIter, Verbose, Prob, Opt, SFinal):- 
-    change(S1, S2),
-    best(Prob, Opt, S1, E1, S2, E2, SN, EN),
-    show(I, Verbose, SN, EN),
+best_opt(_, Opt, S1, E1, S2, E2, SR, ER) :-
+    ((Opt = max, max_list([E1, E2], ER)); (Opt = min, min_list([E1, E2], ER))),
+    ((ER == E1, SR = S1); (ER == E2, SR = S2)).
+
+% Mostrar evolução
+show(final, Verbose, S1, E1, _, _) :-
+    Verbose > 0,
+    write('final:'), write(' S:'), write(S1), write(' E:'), write(E1), nl.
+show(0, Verbose, S1, E1, _, _) :-
+    Verbose > 0,
+    write('init:'), write(' S0:'), write(S1), write(' E0:'), write(E1), nl.
+show(I, Verbose, S1, E1, S2, E2) :-
+    0 is I mod Verbose,
+    write('iter:'), write(I), write(' S1:'), write(S1), write(' E1:'),
+    write(E1), write(' S2:'), write(S2), write(' E2:'), nl.
+show(_, _, _, _, _, _).
+
+% Algoritmo de Hill Climbing
+hill_climbing(S0, [Iter, Verbose, Prob, Opt], S1) :-
+    eval(S0, E0),
+    show(0, Verbose, S0, E0, _, _),
+    hill_climbing(S0, E0, 0, Iter, Verbose, Prob, Opt, S1).
+
+hill_climbing(S, _, Iter, Iter, _, _, _, S).
+hill_climbing(S1, E1, I, Iter, Verbose, Prob, Opt, SFinal) :-
+    change(S1, SNew),
+    best(0, Opt, S1, E1, SNew, _, S2, E2), % Prob = 0 para garantir a escolha da melhor solução
     I1 is I + 1,
-    hill_climbing(SN, EN, I1, MaxIter, Verbose, Prob, Opt, SFinal).
-
-% Demo para testar
-demo_hill_climbing :-
-    caminho(start, end, CaminhoInicial),
-    eval(CaminhoInicial, AvaliacaoInicial),
-    hill_climbing(CaminhoInicial, AvaliacaoInicial, 0, 100, 10, 0.05, min, MelhorCaminho),
-    custoTotal(MelhorCaminho, Custo),
-    calcularTempo(start, MelhorCaminho, Tempo),
-    write('Melhor Caminho: '), write(MelhorCaminho), nl,
-    write('Custo: '), write(Custo), nl,
-    write('Tempo: '), write(Tempo), nl.
+    show(I1, Verbose, S1, E1, S2, E2),
+    hill_climbing(S2, E2, I1, Iter, Verbose, Prob, Opt, SFinal).
